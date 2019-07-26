9ab3927d09844787f2791160a2bd1a884ca6da9f
ColetonS <coleton.m.smith@gmail.com>
2019-07-26 11:40:24 -0600

    finished step 1

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,13 +1,59 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
 export default class Form extends Component {
-    constructor() {
-        super()
-    }
+  constructor() {
+    super();
 
-    render() {
-        return (
-            <div>Form</div>
-        )
-    }
+    this.state = {
+      name: "",
+      price: 0,
+      imgurl: ""
+    };
+  }
+
+  handleNameChange(value) {
+      this.setState({
+          name: value
+      })
+  }
+
+  handlePriceChange(value) {
+      this.setState({
+          price: value
+      })
+  }
+
+  handleImgChange(value) {
+      this.setState({
+          imgurl: value
+      })
+  }
+
+  handleCancel() {
+      this.setState({
+          name: '',
+          price: 0,
+          imgurl: ''
+      })
+
+    //   console.log(this.state)
+  }
+
+  render() {
+    // console.log(this.state)
+    return (
+      <div>
+        <h2>Name</h2>
+        <input onChange={e => this.handleNameChange(e.target.value)} type='text'/>
+        <h2>Price</h2>
+        <input onChange={e => this.handlePriceChange(e.target.value)} type='text'/>
+        <h2>Image URL</h2>
+        <input onChange={e => this.handleImgChange(e.target.value)} type='text'/>
+        <div>
+          <button onClick={() => this.handleCancel()}>Cancel</button>
+          <button>Add to Inventory</button>
+        </div>
+      </div>
+    );
+  }
 }
